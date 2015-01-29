480bd4a61e75da18733f5b215ea980126c0d9bbd
Brian Carreon <brian_carreon@sbcglobal.net>
2015-01-29 13:45:51 -0800

    Weather component added

--- js/clock.js
+++ js/clock.js
@@ -17,4 +17,38 @@ function checkTime(i) {
     return i;
 }
 
+function getTemp() {
+    $.getJSON("https://api.forecast.io/forecast/9f663b2cbd5459e18e6b1b4542d7418f/35.300399,-120.662362?callback=?", setForecast);
+}
+
+function setForecast(data) {
+    $("#forecastLabel").html(data.daily.summary);
+    $("#forecastIcon").attr("src", "images/" + data.currently.icon + ".png");
+
+    var array = data.daily.data;
+    var tempMax = array[0].temperatureMax;
+
+    for (var i = 1; i < array.length; i++) {
+        if (tempMax < array[i].temperatureMax)
+            tempMax = array[i].temperatureMax;
+    }
+
+    if (tempMax < 60)
+        $("body").addClass("cold");
+
+    if (tempMax >= 60)
+        $("body").addClass("chilly");
+
+    if (tempMax >= 70)
+        $("body").addClass("nice");
+
+    if (tempMax >= 80)
+        $("body").addClass("warm");
+
+    if (tempMax >= 90)
+        $("body").addClass("hot");
+}
+
+getTemp();
+
 getTime();
