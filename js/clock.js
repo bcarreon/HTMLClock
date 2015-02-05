93cb1df5621a2687d61b7dfc3133e94fe9acf59b
Brian Carreon <brian_carreon@sbcglobal.net>
2015-02-05 14:44:30 -0800

    Added working alarms and deleting alarms

--- js/clock.js
+++ js/clock.js
@@ -49,6 +49,84 @@ function setForecast(data) {
         $("body").addClass("hot");
 }
 
+function showAlarmPopup() {
+    $("#mask").removeClass("hide");
+    $("#popup").removeClass("hide");
+}
+
+function hideAlarmPopup() {
+    $("#mask").addClass("hide");
+    $("#popup").addClass("hide");
+}
+
+function insertAlarm(hours, mins, ampm, alarmName, id) {
+    var divElement = $("<div id =\"" + id + "\"></div>");
+    var divName = $("<div></div>");
+    var divTime = $("<div></div>");
+    var deleteAlarm = $("<input>");
+
+    deleteAlarm.addClass("button");
+    deleteAlarm.attr("type", "button");
+    deleteAlarm.attr("value", "Delete Alarm");
+    deleteAlarm.attr("onclick", "deleteAlarm(\""+ id + "\")")
+
+    divElement.addClass("flexable");
+    divName.addClass("name").html(alarmName);
+    divTime.addClass("time").html(hours + ":" + mins + " " + ampm);
+
+    divElement.append(divName);
+    divElement.append(divTime);
+    divElement.append(deleteAlarm);
+
+    $("#alarms").append(divElement);
+}
+
+function addAlarm() {
+    var hours = $("#hours option:selected").text();
+    var mins = $("#mins option:selected").text();
+    var ampm = $("#ampm option:selected").text();
+    var alarmName = $("#alarmName").val();
+
+    var AlarmObject = Parse.Object.extend("Alarm");
+    var alarmObject = new AlarmObject();
+
+    alarmObject.save({"hours": hours, "mins": mins, "ampm": ampm, "alarmName": alarmName}, {
+        success: function(object) {
+            insertAlarm(hours, mins, ampm, alarmName, object.id);
+            hideAlarmPopup();
+        }
+    });
+}
+
+function deleteAlarm(id) {
+    $("#" + id).remove();
+
+    var AlarmObject = Parse.Object.extend("Alarm");
+    var alarmObject = new Parse.Query(AlarmObject);
+
+    alarmObject.get(id, {
+        success: function(myObject) {
+            myObject.destroy({});
+        }
+    })
+}
+
+function getAllAlarms() {
+    Parse.initialize("IkN5Q81OcCM19sFxWNeB6s1kwpp42u84koF9krkg", "ZRr2YGU2n90eCxKYt3wVj1X6hWQb5dSGmOO1nQfQ");
+
+    var AlarmObject = Parse.Object.extend("Alarm");
+    var query = new Parse.Query(AlarmObject);
+    query.find({
+        success: function(results) {
+            for (var i = 0; i < results.length; i++) {
+                insertAlarm(results[i].get("hours"), results[i].get("mins"), results[i].get("ampm"), results[i].get("alarmName"), results[i].id);
+            }
+        }
+    });
+}
+
+getAllAlarms();
+
 getTemp();
 
 getTime();
